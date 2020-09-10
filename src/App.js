eb2c46a5f89df98198363b92215a0a21ab22ad7a
BAO LINH <doanbaolinh98@gmail.com>
2020-09-10 21:19:51 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Header from './component/layout/header'
+import Content from './component/content/content'
+function App() {
+  return (
+    <div>
+      <Header />
+      <Content />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/content/Banner.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+export default class Banner extends Component {
+    render() {
+        return (
+            <div className="banner">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-sm-12 col-md-6 col-lg-6 context">
+                            <h5>Bringing You Verified,  <br />
+                            Accredited Specialist
+                            </h5>
+                        </div>
+                        <div className="col-sm-12 col-md-6 col-lg-6 image">
+                            <img src="./images/AdobeStock_94182273.jpeg" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
